a3428a8209b4f1318246c6e1cf44ad07ecc5db44
Ben Limo <bensokiplimo@gmail.com>
2023-03-27 13:56:45 +0300

    add unit test for create company type service

--- src/test/java/com/wapanzi/baristajobsapi/domain/company/service/CompanyTypeServiceImplUnitTest.java
+++ src/test/java/com/wapanzi/baristajobsapi/domain/company/service/CompanyTypeServiceImplUnitTest.java
@@ -2,7 +2,6 @@ package com.wapanzi.baristajobsapi.domain.company.service;
 
 import com.wapanzi.baristajobsapi.domain.company.model.CompanyType;
 import com.wapanzi.baristajobsapi.domain.company.repository.CompanyTypeRepository;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,23 +10,32 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
-
 @SpringBootTest(webEnvironment = NONE)
-public class CompanyTypeServiceImplIntegrationTest {
+public class CompanyTypeServiceImplUnitTest {
     @Autowired
-    private CompanyTypeServiceImpl companyTypeServiceImpl;
+    private CompanyTypeService companyTypeService;
+    @MockBean
+    private CompanyTypeRepository companyTypeRepository;
 
     @Test
     void testCreateCompanyType_returnCreatedCompanyType() {
         // given
-        CompanyType companyType = new CompanyType(null, "Barista", LocalDateTime.now(), LocalDateTime.now());
+        CompanyType companyType =  new CompanyType(1l, "Barista", LocalDateTime.now(), LocalDateTime.now());
+        given(companyTypeRepository.save(any(CompanyType.class))).willReturn(
+                companyType
+        );
 
         // when
-        CompanyType savedCompanyType = companyTypeServiceImpl.createNewCompanyType(companyType);
+        CompanyType savedCompanyType = companyTypeService.createNewCompanyType(
+                new CompanyType(3l, "Bar", LocalDateTime.now(), LocalDateTime.now())
+        );
 
         // then
         then(savedCompanyType.getId()).isNotNull();
         then(savedCompanyType.getCompanyType()).isEqualTo("Barista");
+
     }
 }
