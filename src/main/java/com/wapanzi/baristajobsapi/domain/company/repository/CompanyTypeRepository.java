5e4e4284a362893dda4f53651d02dd9a79305d3a
Ben Limo <bensokiplimo@gmail.com>
2023-03-27 11:36:02 +0300

    move company type repository

--- src/main/java/com/wapanzi/baristajobsapi/domain/company/repository/CompanyTypeRepository.java
+++ src/main/java/com/wapanzi/baristajobsapi/domain/company/repository/CompanyTypeRepository.java
@@ -1,4 +1,4 @@
-package com.wapanzi.baristajobsapi.domain.company.model.repository;
+package com.wapanzi.baristajobsapi.domain.company.repository;
 
 import com.wapanzi.baristajobsapi.domain.company.model.CompanyType;
 import org.springframework.data.jpa.repository.JpaRepository;
