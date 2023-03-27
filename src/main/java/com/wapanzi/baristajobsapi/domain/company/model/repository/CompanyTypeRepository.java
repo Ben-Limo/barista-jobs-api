b75a976f6abdacb92efc3b86f6edaff14a033135
Ben Limo <bensokiplimo@gmail.com>
2023-03-27 11:32:25 +0300

    add company type repository

---
+++ src/main/java/com/wapanzi/baristajobsapi/domain/company/model/repository/CompanyTypeRepository.java
@@ -0,0 +1,7 @@
+package com.wapanzi.baristajobsapi.domain.company.model.repository;
+
+import com.wapanzi.baristajobsapi.domain.company.model.CompanyType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyTypeRepository extends JpaRepository<CompanyType, Long> {
+}
