7c2b24f610d93dc545e8b10a879f1d00b504be26
Ben Limo <bensokiplimo@gmail.com>
2023-03-27 10:25:53 +0300

    add company type model

---
+++ src/main/java/com/wapanzi/baristajobsapi/domain/company/model/CompanyType.java
@@ -0,0 +1,37 @@
+package com.wapanzi.baristajobsapi.domain.company.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CompanyType {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    @Size(min = 2,  message = "Company type should have at least 2 characters")
+    private String companyType;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+}
